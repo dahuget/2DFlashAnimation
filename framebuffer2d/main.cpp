b3f330d7477acb121a608d27151886ade84c933d
dahuget <danaliyahuget@gmail.com>
2018-03-13 21:26:23 -0700

    changed color texture names

--- framebuffer2d/main.cpp
+++ framebuffer2d/main.cpp
@@ -40,11 +40,11 @@ std::unique_ptr<Shader> lineShader;
 std::unique_ptr<GPUMesh> line;
 std::vector<Vec2> controlPoints;
 
-std::unique_ptr<RGBA8Texture> cat;
+std::unique_ptr<RGBA8Texture> moto;
 std::unique_ptr<RGBA8Texture> wheel1;
 std::unique_ptr<RGBA8Texture> wheel2;
 std::unique_ptr<RGBA8Texture> tail;
-std::unique_ptr<RGBA8Texture> stars;
+std::unique_ptr<RGBA8Texture> saltflats;
 
 /// TODO: declare Framebuffer and color buffer texture
 std::unique_ptr<Framebuffer> fb;
@@ -115,11 +115,11 @@ void init(){
 
     quadInit(quad);
 
-    loadTexture(cat, "moto.png");
+    loadTexture(moto, "moto.png");
     loadTexture(wheel1, "wheel.png");
     loadTexture(wheel2, "wheel.png");
     loadTexture(tail, "sparkle.png");
-    loadTexture(stars, "saltflats.png");
+    loadTexture(saltflats, "saltflats.png");
 
     lineShader = std::unique_ptr<Shader>(new Shader());
     lineShader->verbose = true;
@@ -191,28 +191,27 @@ void drawScene(float timeCount)
 
     float t = timeCount * SpeedFactor;
 
+    // transformation matrix
     Transform TRS = Transform::Identity();
 
+    // background
     quadShader->bind();
     quadShader->set_uniform("M", TRS.matrix());
     // Make texture unit 0 active
     glActiveTexture(GL_TEXTURE0);
     // Bind the texture to the active unit for drawing
-    stars->bind();
+    saltflats->bind();
     // Set the shader's texture uniform to the index of the texture unit we have
     // bound the texture to
     quadShader->set_uniform("tex", 0);
     quad->set_attributes(*quadShader);
     quad->draw();
-    stars->unbind();
+    saltflats->unbind();
 
-    float xcord = 0.7*std::cos(t);
-    float ycord = 0.7*std::sin(t);
-    //TRS *= Eigen::Translation3f(xcord, ycord, 0);
-    //TRS *= Eigen::AngleAxisf(t + M_PI / 2, Eigen::Vector3f::UnitZ());
     TRS *= Eigen::AlignedScaling3f(0.38f, 0.2f, 1.0);
 
-    // transform along bezier curve (bottom left to top right curve)
+    // transform along bezier curve
+    // bottom left to top right curve
     /*Vec2 P0 = Vec2(-4.0f,-4.0f);
     Vec2 P1 = Vec2(-4.0f, -3.0f);
     Vec2 P2 = Vec2( 4.0f, 7.0f);
@@ -238,34 +237,30 @@ void drawScene(float timeCount)
     Vec2 P2 = Vec2(0.0f, 4.0f);
     Vec2 P3 = Vec2(4.5f, 4.5f);
 
-
-    float scale_t = timeCount*0.3;
+    // calculate bezier curve
+    float scale_t = t*0.3;
     scale_t = fmod(scale_t, 1.0);
     float x = pow((1-scale_t), 3) *P0(0) + 3*scale_t*pow((1-scale_t),2)*P1(0) + 3*pow(scale_t,2)*(1-scale_t)*P2(0) + pow(scale_t,3)*P3(0);
     float y = pow((1-scale_t), 3) *P0(1) + 3*scale_t*pow((1-scale_t),2)*P1(1) + 3*pow(scale_t,2)*(1-scale_t)*P2(1) + pow(scale_t,3)*P3(1);
-    //TRS *= Eigen::Translation3f(x, y, 0.0);
+    TRS *= Eigen::Translation3f(x, y, 0.0);
 
-    // add heirarchies of transformation
-    // **** 1. wheels spinning
-    float freq = M_PI*timeCount*1.0/*SPEED_FACTOR*/;
+    // **** add heirarchies of transformation
+    // **** 1. two wheels spinning
+    float freq = M_PI*t;
     Transform wheel1_TRS = TRS;
     wheel1_TRS *= Eigen::AlignedScaling3f(0.4f, 0.68f, 1.0);
     wheel1_TRS *= Eigen::Translation3f(-1.8, -0.4, 0.0);
-    wheel1_TRS *= Eigen::AngleAxisf(-freq/30/*SUN_ROT_PERIOD*/, Eigen::Vector3f::UnitZ());
+    wheel1_TRS *= Eigen::AngleAxisf(-freq/*spins clockwise*/, Eigen::Vector3f::UnitZ());
 
     Transform wheel2_TRS = TRS;
     wheel2_TRS *= Eigen::AlignedScaling3f(0.4f, 0.68f, 1.0);
     wheel2_TRS *= Eigen::Translation3f(1.8, -0.4, 0.0);
-    wheel2_TRS *= Eigen::AngleAxisf(-freq/30/*SUN_ROT_PERIOD*/, Eigen::Vector3f::UnitZ());
+    wheel2_TRS *= Eigen::AngleAxisf(-freq/*spins clockwise*/, Eigen::Vector3f::UnitZ());
 
-    // **** 2. sparkle tail
+    // **** 2. sparkle tail becomes bigger and smaller over time
     Transform tail_TRS = TRS;
     tail_TRS *= Eigen::Translation3f(-1.83, 0.3, 0.0);
-    //tail_TRS *= Eigen::AlignedScaling3f(0.9, 0.9, 1.0);
-    //scale_t: make the sun become bigger and smaller over the time!
-    scale_t = 0.1*std::sin(t); // normalized percentage [0,1)
-    //std::cout << scale_t << std::endl;
-    //tail_TRS *= Eigen::Translation3f(-1.85+scale_t, 0.0, 0.0);
+    scale_t = 0.2*std::sin(t); // normalized percentage [0,1)
     tail_TRS *= Eigen::AlignedScaling3f(1.0+scale_t, 1.0+scale_t, 1.0);
 
     quadShader->bind();
@@ -287,13 +282,13 @@ void drawScene(float timeCount)
     // Make texture unit 0 active
     glActiveTexture(GL_TEXTURE0);
     // Bind the texture to the active unit for drawing
-    cat->bind();
+    moto->bind();
     // Set the shader's texture uniform to the index of the texture unit we have
     // bound the texture to
     quadShader->set_uniform("tex", 0);
     quad->set_attributes(*quadShader);
     quad->draw();
-    cat->unbind();
+    moto->unbind();
     quadShader->unbind();
 
     quadShader->bind();
